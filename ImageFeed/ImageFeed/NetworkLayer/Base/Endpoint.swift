41fdc9dcf2d66608ad60a97d2b6dc25c6bc8d6f3
Moein Barzegaran <barzegaranmoein@gmail.com>
2024-07-30 13:11:27 +0200

    Define base structure for network layer

---
+++ ImageFeed/ImageFeed/NetworkLayer/Base/Endpoint.swift
@@ -0,0 +1,27 @@
+//
+//  Endpoint.swift
+//  ImageFeed
+//
+//  Created by Moein Barzegaran on 7/30/24.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+}
+
+extension Endpoint {
+    var scheme: String {
+        return "https"
+    }
+
+    var host: String {
+        return "api.pexels.com"
+    }
+}
---
+++ ImageFeed/ImageFeed/NetworkLayer/Base/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  ImageFeed
+//
+//  Created by Moein Barzegaran on 7/30/24.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
